4bd85c5b76c239f07321f1fc40f8911a6f958a30
matt-wilshaw <m.wilshaw@outlook.com>
2025-11-11 20:29:11 +0000

    Removed all instances of import 'Note from API models and swapped to SQLite for testing, migration now works

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Note
+
 
 # Serializer for the User model
 # - Converts User instances to JSON and vice versa
@@ -15,16 +15,6 @@ class UserSerializer(serializers.ModelSerializer):
     # Override create method to use Django's create_user
     # This ensures the password is hashed properly
     def create(self, validated_data):
-        print(validated_data)  # For debugging: shows incoming data
         user = User.objects.create_user(**validated_data)
         return user
 
-
-# Serializer for the Note model
-# - Converts Note instances to JSON and vice versa
-# - Author is read-only because it will be set automatically from request.user
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Note
-        fields = ["id", "title", "content", "created_at", "author"]
-        extra_kwargs = {"author": {"read_only": True}}
