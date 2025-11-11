dffbd9c418f7986dbfd50a87e264314427741b15
matt-wilshaw <m.wilshaw@outlook.com>
2025-11-11 18:02:29 +0000

    Add URL routes for GymFlex API, JWT auth, and admin

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+
+
+# View to create a new user
+# - Uses UserSerializer to handle user creation
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
