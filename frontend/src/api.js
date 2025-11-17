8ab8a9de7929a0690ef02f59f1782c4dc358d146
matt-wilshaw <m.wilshaw@outlook.com>
2025-11-17 15:02:32 +0000

    Added comments to frontend api.js code

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,36 @@
+// Import the axios library for making HTTP requests
 import axios from "axios";
+
+// Import a constant representing the key used for storing the access token in localStorage
 import { ACCESS_TOKEN } from "./constants.js";
 
+// Create an instance of axios with a default base URL
+// The base URL is read from environment variables (Vite project)
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL // e.g., "http://localhost:8000/api"
+});
 
+// Add a request interceptor to automatically include the Authorization header
+// This runs before every request made with this axios instance
 api.interceptors.request.use(
     (config) => {
+        // Get the access token from localStorage
         const token = localStorage.getItem(ACCESS_TOKEN);
+
+        // If a token exists, attach it to the request headers
+        // as a Bearer token for JWT authorisation
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
         }
+
+        // Return the updated config to continue the request
         return config;
     },
     (error) => {
+        // If there was an error setting up the request, reject the promise
         return Promise.reject(error);
     }
 );
 
-export default api
+// Export the configured axios instance so it can be used throughout the project
+export default api;
