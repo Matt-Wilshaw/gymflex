be5a76029147cec906ac430773003b93ef471d4e
matt-wilshaw <m.wilshaw@outlook.com>
2025-11-17 17:37:44 +0000

    Created initial pages for managing login and added routing logic to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
 
 
