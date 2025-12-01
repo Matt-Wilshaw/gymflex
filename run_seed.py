530eb507368b43ff5e20f492bd99f33d0f7d1d93
matt-wilshaw <m.wilshaw@outlook.com>
2025-12-01 15:38:49 +0000

    Fix run_seed.py path handling

--- run_seed.py
+++ run_seed.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import os
+import sys
 import django
+
+# Add backend directory to path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
 # Import and run the seed script
-import sys
-sys.path.insert(0, 'backend')
 from seed_sessions import run
 
 if __name__ == '__main__':
